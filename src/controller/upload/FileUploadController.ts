d380abaefd7cf14fd06b152b2ccf4a3a50f8a000
devJKLee <beneciajk@gmail.com>
2019-10-22 11:14:58 +0900

    1) 파일 업로드 컨트롤러에 ORM ( Sequelize ) 적용 2) FileVO 를 Sequelize 적용시 선언했던, FileDB 모델로 대체 3) error 발생시 로그 남기는 기능 추가

--- src/controller/upload/FileUploadController.ts
+++ src/controller/upload/FileUploadController.ts
@@ -3,16 +3,28 @@
  * date: 2018-12-10
  */
 
-import {MysqlError} from "mysql";
-import {FileVO} from "../../VO/FileVO";
-import {Service} from "../../service/Service";
-import {Query} from "../../service/Query";
 import {Guid} from "../../util/Guid";
 import * as express from 'express';
 import {ThumbnailController} from "./ThumbnailController";
+import {FileDB} from "../../models/FileDB";
+import {WinstonLogger} from "../../log/WinstonLogger";
 
 export class FileUploadController
 {
+
+    /**
+     * DB 커넥션 과정에서 에러가 발생했을 시, 로그를 남기고 에러 처리한다.
+     * 만약, ajax 를 통한 통신 요청이었다면, 500 error 를 반환하고,
+     * 관리자 페이지 내에서 발생한 에러라면 404 error 처리한다.
+     * @param error
+     * @param {e.Response} res
+     */
+    private static sendResponseError(req:express.Request, res:express.Response, error:any):void
+    {
+        WinstonLogger.error(error);
+        if(req.xhr) res.status(500).send({status:500, message:'internal error', type:'internal'});
+        else res.render('404');
+    }
 
     /**
      * 업로드 페이지 요청
@@ -40,7 +52,7 @@ export class FileUploadController
     {
         let fileList:Array<Object> = req.files as Array<Object>;
         let guid = req.body.guid;
-        if(fileList.length != 0) this.fileUploadProcess(fileList, guid, res);
+        if(fileList.length != 0) this.fileUploadProcess(fileList, guid, req, res);
     }
 
     /**
@@ -48,22 +60,23 @@ export class FileUploadController
      * @param {Array<Object>} fileSource    첨부 파일 데이터
      * @param {string} key                  해당 파일이 첨부된 URL의 키값
      */
-    public static fileUploadProcess(fileSource:Array<Object>, guid:Guid, res:express.Response):void
+    public static fileUploadProcess(fileSource:Array<Object>, guid:string, req:express.Request, res:express.Response):void
     {
         for(let num = 0; num < fileSource.length; num++)
         {
-            let fileVO:FileVO = fileSource[num] as FileVO;
+            let fileVO:FileDB = fileSource[num] as FileDB;
             fileVO.guid = guid;
-            console.log(fileVO);
-            Service.queryExecution(Query.FILEUPLOAD, fileVO, (err:MysqlError, results:any)=>{ if(err) throw err; this.fileUploadProcessComplete(fileVO, res); });
+            let queryResults = FileDB.create(fileVO);
+            queryResults.then(() => { this.fileUploadProcessComplete(fileVO, res); });
+            queryResults.catch((error:any)=>{ this.sendResponseError(req, res, error); });
         }
     }
 
     /**
      * 해당 파일의 DB 업로드가 종료되면, 해당 파일의 이미지 타입을 체크해서 썸네일을 제작한다.
-     * @param {FileVO} file
+     * @param {FileDB} file
      */
-    private static fileUploadProcessComplete(file:FileVO, res:express.Response):void
+    private static fileUploadProcessComplete(file:FileDB, res:express.Response):void
     {
         this.imageTypeCheck(file);
         res.sendStatus(200);
@@ -71,15 +84,12 @@ export class FileUploadController
 
     /**
      * 파일의 타입을 체크해서, 이미지 형태의 파일(확장자 'png', 'jpg', 'jpeg', 'gif')이면 썸네일을 생성한다.
-     * @param {FileVO} file 썸네일을 생성하려는 파일
+     * @param {FileDB} file 썸네일을 생성하려는 파일
      */
-    public static imageTypeCheck(file:FileVO):void
+    public static imageTypeCheck(file:FileDB):void
     {
         var imageType = /image.*/;
-        if (file.mimetype.match(imageType))
-        {
-            ThumbnailController.makeThumbnail(file.filename, 400, 300);
-        }
+        if (file.mimetype.match(imageType)) ThumbnailController.makeThumbnail(file.filename, 400, 300);
     }
 
 }
